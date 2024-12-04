411028cc252f2385febdd28db6acf38f2367c543
Sukhbinder Singh <sukhbindersingh@gmail.com>
2024-12-04 20:12:09 +0530

    Add new characters

--- tests/test_winzy_visual_notify.py
+++ tests/test_winzy_visual_notify.py
@@ -1,7 +1,8 @@
 import pytest
 import winzy_visual_notify as w
+from winzy_visual_notify.pyside6_speech_bubble import get_character_choices
 
-from argparse import Namespace, ArgumentParser
+from argparse import ArgumentParser
 
 def test_create_parser():
     subparser = ArgumentParser().add_subparsers()
@@ -18,3 +19,10 @@ def test_plugin(capsys):
     w.tell_plugin.hello(None)
     captured = capsys.readouterr()
     assert "Hello! This is an example ``winzy`` plugin." in captured.out
+
+
+def test_get_character_choices():
+    choices = get_character_choices()
+    assert len(choices) == 9
+    assert "dog2" in choices
+    assert "watermellon" in choices
